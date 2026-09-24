3633f5feac4f568ed90ce321a8ec873308a730d9
guptasagr <78406871+guptasagr@users.noreply.github.com>
2026-09-24 21:23:21 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ram-naam-v3';
+const CACHE='ram-naam-safe-v1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./ram.mp3'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
